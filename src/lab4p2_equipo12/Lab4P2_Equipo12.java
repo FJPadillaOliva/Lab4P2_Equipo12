6f5d6699958a5d3ffea32cd7e75cb68dd0449301
FJPadillaOliva <fjpo@unitec.edu>
2023-08-11 14:14:00 -0600

    Menu con switch/case

--- src/lab4p2_equipo12/Lab4P2_Equipo12.java
+++ src/lab4p2_equipo12/Lab4P2_Equipo12.java
@@ -1,12 +1,42 @@
-
 package lab4p2_equipo12;
 
+import java.util.Scanner;
 
 public class Lab4P2_Equipo12 {
 
-    
+    static Scanner read = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        int opcion = 0;
+        do {
+            System.out.println("----------Menu----------\n"
+                    + "1.Registrar Entrenador\n"
+                    + "2.Battle Factory\n"
+                    + "3.Capturar/Entrenar\n"
+                    + "4.Añadir Movimiento\n"
+                    + "5.Salir");
+            opcion = read.nextInt();
+            switch(opcion){
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    opcion = 5;
+                    break;
+                default :
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        } while (opcion != 5);
     }
-    
+
 }
